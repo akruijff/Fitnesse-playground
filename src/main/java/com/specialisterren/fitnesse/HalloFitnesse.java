f6132070b6fd3ed15a3188d4460d2aa7a25db573
Alex de Kruijff <alexk@specialisterren.nl>
2015-10-15 15:37:35 +0200

    Example

---
+++ src/main/java/com/specialisterren/fitnesse/HalloFitnesse.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.specialisterren.fitnesse;
+
+/**
+ *
+ * @author Alex de Kruijff <alex.de.kruijff@specialisterren.nl>
+ */
+public class HalloFitnesse {
+
+    public boolean halloWorld(String arg, String arg2, String arg3) {
+        return true;
+    }
+}
